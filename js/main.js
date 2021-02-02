77e6ec3619214e0e8bbc659c7a259e782a64cfff
Andrii Stepanets <ovrass@gmail.com>
2021-02-02 13:24:23 +0200

    Animated nav links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 gsap.registerPlugin(ScrollTrigger);
 
-function init(){
-    
-    // start here
+// this func need to be animated after hover link effect
+function initNavigation() {
+    const mainNavLinks = gsap.utils.toArray('.main-nav a');
+    mainNavLinks.forEach(link => link.addEventListener('mouseleave', e => {
+        link.classList.add('animate-out');
+        setTimeout(() => {
+            link.classList.remove('animate-out')
+        }, 300);
+    }))
+}
+
+function init() {
+
+    initNavigation()
 
 }
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     init();
 });
