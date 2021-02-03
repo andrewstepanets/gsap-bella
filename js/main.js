4d7bb56c15f10346276248996d6d105849356540
Andrii Stepanets <ovrass@gmail.com>
2021-02-03 18:52:35 +0200

    Finished header section animation

--- js/main.js
+++ js/main.js
@@ -56,7 +56,47 @@ function moveImages(e) {
     const { offsetX, offsetY, target } = e;
     const { clientWidth, clientHeight } = target;
 
-    console.log(offsetX, offsetY, clientWidth, clientHeight);
+    const xPos = (offsetX / clientWidth) - 0.5;
+    const yPos = (offsetY / clientHeight) - 0.5;
+
+    // get 0,0 in the center
+
+    const leftImages = gsap.utils.toArray('.hg__left .hg__image');
+    const rightImages = gsap.utils.toArray('.hg__right .hg__image');
+
+    // add modifier func to get variety data x and y
+    const modifier = (index) => index * 1.2 + 0.5;
+
+    // move left 3 images
+    leftImages.forEach((image, index) => {
+        gsap.to(image, {
+            duration: 1.2,
+            x: xPos * 20 * modifier(index),
+            y: yPos * 30 * modifier(index),
+            rotationY: xPos * 40,
+            rotationX: yPos * 10,
+            ease: 'power3.out'
+
+        })
+    })
+    // move right 3 images
+    rightImages.forEach((image, index) => {
+        gsap.to(image, {
+            duration: 1.2,
+            x: xPos * 20 * modifier(index),
+            y: -yPos * 30 * modifier(index),
+            rotationY: xPos * 40,
+            rotationX: yPos * 10,
+            ease: 'power3.out'
+        })
+    })
+    // animate decor circle
+    gsap.to('.decor__circle', {
+        duration: 1.7,
+        x: 100 * xPos,
+        y: 120 * yPos,
+        ease: 'power4.out'
+    })
 }
 
 function initHeaderTilt() {
