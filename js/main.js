a316bf1161afa59661d4711978d145393ce0dddd
Andrii Stepanets <ovrass@gmail.com>
2021-02-03 13:17:02 +0200

    Changed settimeout to transitionend event

--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@ gsap.registerPlugin(ScrollTrigger);
 // this func need to be animated after hover link effect
 function initNavigation() {
     const mainNavLinks = gsap.utils.toArray('.main-nav a');
-    mainNavLinks.forEach(link => link.addEventListener('mouseleave', e => {
-        link.classList.add('animate-out');
-        setTimeout(() => {
-            link.classList.remove('animate-out')
-        }, 300);
-    }))
+    mainNavLinks.forEach(link => {
+        link.addEventListener('mouseleave', e => {
+            //add class
+            link.classList.add('animate-out');
+            // setTimeout(() => {
+            //     link.classList.remove('animate-out')
+            // }, 300);
+        });
+        link.ontransitionend = function () {
+            //remove class
+            link.classList.remove('animate-out');
+        }
+
+    })
 }
 
 function init() {
